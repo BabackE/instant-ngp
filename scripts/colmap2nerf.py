e8ca070d9481251ab4b96a97bb56cf3e82c66eaa
Baback <baback@cs.washington.edu>
2024-04-22 20:32:33 -0700

    ignore empty lines for dust3r exports

--- scripts/colmap2nerf.py
+++ scripts/colmap2nerf.py
@@ -335,7 +335,7 @@ if __name__ == "__main__":
 		up = np.zeros(3)
 		for line in f:
 			line = line.strip()
-			if line[0] == "#":
+			if len(line) > 0 and line[0] == "#":
 				continue
 			i = i + 1
 			if i < SKIP_EARLY*2:
